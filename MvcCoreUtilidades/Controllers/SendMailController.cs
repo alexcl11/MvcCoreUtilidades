926c5dcb293e3836be03c621f8e3134e694f9618
alexcl11 <aleecanovaslopez@gmail.com>
2026-02-23 12:58:06 +0100

    UP

---
+++ MvcCoreUtilidades/Controllers/SendMailController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class SendMailController : Controller
+    {
+        private IConfiguration configuration;
+
+        public SendMailController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMail(string to, string asunto, string mensaje)
+        {
+            string user = this.configuration.GetValue<string>("MailSettings:Credentials:User");
+            // OBJETO PARA LA INFORMACION DEL MAIL
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(user);
+            mail.To.Add(to);
+            mail.Subject = asunto;
+            mail.Body = mensaje;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.Normal;
+            // RECUPERAMOS LOS DATOS PARA EL OBJETO QUE MANDA EL PROPIO MAIL
+            string password = this.configuration.GetValue<string>("MailSettings:Credentials:Password");
+            string host = this.configuration.GetValue<string>("MailSettings:Server:Host");
+            int port = this.configuration.GetValue<int>("MailSettings:Server:Port");
+            bool ssl = this.configuration.GetValue<bool>("MailSettings:Server:Ssl");
+            bool defaultCredentials = this.configuration.GetValue<bool>("MailSettings:Server:DefaultCredentials");
+            SmtpClient client = new SmtpClient();
+            client.Host = host;
+            client.Port = port;
+            client.EnableSsl = ssl;
+            client.UseDefaultCredentials = defaultCredentials;
+            //CREDENCIALES PARA EL MAIL
+            NetworkCredential credentials = new NetworkCredential(user, password);
+            await client.SendMailAsync(mail);
+            ViewData["MENSAJE"] = "Mensaje enviado correctamente";
+            return View();
+        }
+    }
+}
---
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.ObjectPool;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    public class HelperCryptography
+    {
+        // CREAMOS UN STRING PARA EL SALT
+        public static string Salt { get; set; }
+        // METODO PARA GENERAR UN SALT ALEATORIO
+        private static string GenerateSalt()
+        {
+            Random random = new Random();
+            string salt = "";
+            for (int i = 0; i <= 30; i++)
+            {
+                //GENERAMOS UN ALEATORIO
+                int num = random.Next(1, 255);
+                char letra = Convert.ToChar(num);
+                salt += letra;
+            }
+            return salt;
+        }
+
+        // CREAMOS UN METODO EFICIENTE PARA DESCIFRAR
+        public static string CifrarContenido(string contenido, bool comparar)
+        {
+            if (!comparar)
+            {
+                // NO QUEREMOS COMPARAR, SOLO CIFRADO
+                // CREAMOS UN NUEVO SALT
+                Salt = GenerateSalt();
+            }
+            // REALIZAMOS EL CIFRADO
+            string contenidoSalt = contenido + Salt;
+            // UTILIZAMOS EL OBJETO GRANDE PARA CIFRAR
+            SHA512 managed = SHA512.Create();
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] salida;
+            salida = encoding.GetBytes(contenidoSalt);
+            // REALIZAR n ITERACIONES SOBRE EL PROPIO CIFRADO
+            for (int i = 0; i <= 21; i++)
+            {
+                // CIFRADO SOBRE CIFRADO
+                salida = managed.ComputeHash(salida);
+            }
+            //DEBEMOS LIBERAR LA MEMORIA
+            managed.Clear();
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+
+        //CREAMOS LOS METODOS TIPO STATIC
+        // SIMPLEMENTE DEVOLVEMOS UN TXTO CIFRADO
+
+        public static string EncriptarTextoBasico(string contenido)
+        {
+            //EL CIFRADO SE REALIZA A NIVEL DE BYTES
+            // DEBEMOS CONVERTIR EL TEXTO DE ENTRADA A BYTES
+            byte[] entrada;
+            // DESPUES DE CIFRAR LOS BYTES, NOS DARA UNA SALIDA DE BYTES[]
+            byte[] salida;
+            // NECESITAMOS UNA CLASE PARA CONVERTIR DE BYTE[] A STRING Y VICEVERSA
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            // NECESITAMOS UN OBJETO PARA CIFRAR EL CONTENIDO
+            SHA1 managed = SHA1.Create();
+            // CONVERTIMOS EL TEXTO A BYTES[]             
+            entrada = encoding.GetBytes(contenido);
+            // LOS OBJETOS DE CIFRADO TIENEN UN METODO LLAMADO 
+            // ComputeHash() QUE RECIBE UN ARRAY DE BYTES, REALIZAN ACCIONES INTERNAS
+            // Y DEVUELVEN EL ARRAY BYTES[] CIFRADO
+            salida = managed.ComputeHash(entrada);
+            // CONVERTIMOS LOS BYTES A TEXTO
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+    }
+}
